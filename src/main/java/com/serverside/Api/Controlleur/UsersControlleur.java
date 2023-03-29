ec7102094c9d87dc3f2c913b107984cd13f533ee
echchabli_hamza <echchablihamza1@gmail.com>
2023-03-29 22:16:46 +0000

    commit A

--- src/main/java/com/serverside/Api/Controlleur/UsersControlleur.java
+++ src/main/java/com/serverside/Api/Controlleur/UsersControlleur.java
@@ -37,6 +37,8 @@ public class UsersControlleur {
         return userdao.getAll();
 
     }
+
+
     @GetMapping("/users/getOne/{id}")
     public Users_data getOne(@PathVariable Long id){
        return userdao.getOne(id);
